31ee0ce629c663acf71ae5decf178e8bda319534
rifouin <arse721@gmail.com>
2016-01-22 12:46:18 +0200

    test message

--- src/com/utype/Main.java
+++ src/com/utype/Main.java
@@ -6,5 +6,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hello again!");
         System.out.println("I love you, world!");
+        System.out.println("never gonna give you up");
     }
 }
