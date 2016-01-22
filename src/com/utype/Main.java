f01dbb25811e46bd84bdfbc769550764f6ee5d51
anton5 <anton5@example.com>
2016-01-22 15:00:53 +0200

    Testing Git 2

--- src/com/utype/Main.java
+++ src/com/utype/Main.java
@@ -1,13 +1,10 @@
 package com.utype;
-// some comment, just for testing
+
+import com.utype.AntonGame;
 
 public class Main {
-
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("Hello again!");
-        System.out.println("I love you, world!");
-        System.out.println("never gonna give you up");
-eafd
+        AntonGame g = new AntonGame();
+        g.run();
     }
 }
