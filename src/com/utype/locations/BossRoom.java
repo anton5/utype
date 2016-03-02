c78766f308f57c9c954487445daf0e41a055758c
Roma Laitarenko <sfm.kmr@gmail.com>
2016-03-02 14:19:44 +0200

    Using correct method to output final message.

--- src/com/utype/locations/BossRoom.java
+++ src/com/utype/locations/BossRoom.java
@@ -1,14 +1,24 @@
 package com.utype.locations;
+
 import com.utype.Logger;
+import com.utype.characters.Character;
 
 public class BossRoom extends Location {
 
     public BossRoom(String name) {
         super(name);
     }
+
     @Override
-    protected void startPuzzleIfNeeded() {
+    public void onCharacterDidEnter(Character character) {
+        super.onCharacterDidEnter(character);
+
         Logger.logln("You've reached the boss room. You won!");
+    }
+
+    @Override
+    protected void rollTheMonster() {
+        // we don't have final fight currently
     }
 
     @Override
